d3064c14ebb767bde970daabd4ced76f7b2ca6ca
stpauls-modding <23203@stpauls.qld.edu.au>
2015-10-09 10:36:44 +1000

    Reset

--- src/com/stpaulsmodding/csplugin/CSPlugin.java
+++ src/com/stpaulsmodding/csplugin/CSPlugin.java
@@ -1,9 +1,7 @@
 package com.stpaulsmodding.csplugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CSPlugin extends JavaPlugin {
@@ -19,12 +17,7 @@ public class CSPlugin extends JavaPlugin {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("smiteall")) {
-			for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-				player.getWorld().strikeLightning(player.getLocation());
-				player.setHealth(0.0D);
-			}
-		}
+		
 		
 		return false;
 	}
