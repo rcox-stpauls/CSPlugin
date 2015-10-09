9ea265d3726432b7328d06717c183b8ff44e40b9
23203@stpauls.qld.edu.au <connectmerob@live.com>
2015-10-09 20:13:28 +1000

    UPDAAAATE

--- src/com/stpaulsmodding/csplugin/EventListener.java
+++ src/com/stpaulsmodding/csplugin/EventListener.java
@@ -1,7 +1,20 @@
 package com.stpaulsmodding.csplugin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class EventListener implements Listener {
 	
+	@EventHandler
+	public void onDamage(EntityDamageEvent event) {
+		if (event.getEntity() instanceof Player) {
+			
+			Player player = (Player) event.getEntity();
+			if () {
+				
+			}
+		}
+	}
 }
