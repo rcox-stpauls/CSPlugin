0b46aa528bd359f908b5493ec67dd846692fa518
23203@stpauls.qld.edu.au <connectmerob@live.com>
2015-10-09 21:41:46 +1000

    Functionality

--- src/com/stpaulsmodding/csplugin/EventListener.java
+++ src/com/stpaulsmodding/csplugin/EventListener.java
@@ -4,16 +4,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
 	
 	@EventHandler
 	public void onDamage(EntityDamageEvent event) {
 		if (event.getEntity() instanceof Player) {
-			
 			Player player = (Player) event.getEntity();
-			if () {
-				
+			if (event.getCause().equals(DamageCause.FIRE)) {
+				for (ItemStack set : player.getInventory().getArmorContents()) {
+					switch(set.getType()) {
+						case LEATHER_BOOTS:
+							set.setDurability((short) (set.getDurability() - 5));
+							break;
+						case LEATHER_LEGGINGS:
+							set.setDurability((short) (set.getDurability() - 6));
+							break;
+						case LEATHER_CHESTPLATE:
+							set.setDurability((short) (set.getDurability() - 6));
+							break;
+						case LEATHER_HELMET:
+							set.setDurability((short) (set.getDurability() - 4));
+							break;
+						default:
+							break;
+					}	
+				}
 			}
 		}
 	}
