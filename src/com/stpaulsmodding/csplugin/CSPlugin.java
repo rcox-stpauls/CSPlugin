432b5d652e6778a56150f4827f176d0f80c78819
23203@stpauls.qld.edu.au <connectmerob@live.com>
2015-10-09 19:46:57 +1000

    Extra files

--- src/com/stpaulsmodding/csplugin/CSPlugin.java
+++ src/com/stpaulsmodding/csplugin/CSPlugin.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CSPlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		
+		getServer().getPluginManager().registerEvents(new EventListener(), this);
 	}
 	
 	@Override
---
+++ src/com/stpaulsmodding/csplugin/PluginCommands.java
@@ -0,0 +1,19 @@
+package com.stpaulsmodding.csplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PluginCommands implements CommandExecutor {
+	@SuppressWarnings("unused")
+	private final CSPlugin plugin;
+	
+	public PluginCommands(CSPlugin plugin) {
+		this.plugin = plugin;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
+}
