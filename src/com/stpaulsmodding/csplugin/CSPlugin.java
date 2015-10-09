dd6a6ab6c7b7e8c12ca8348d5afe7332abcc7a52
23203@stpauls.qld.edu.au <connectmerob@live.com>
2015-10-09 18:24:53 +1000

    Update

--- src/com/stpaulsmodding/csplugin/CSPlugin.java
+++ src/com/stpaulsmodding/csplugin/CSPlugin.java
@@ -1,7 +1,5 @@
 package com.stpaulsmodding.csplugin;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CSPlugin extends JavaPlugin {
@@ -13,12 +11,5 @@ public class CSPlugin extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
-		
-		return false;
 	}
 }
---
+++ src/com/stpaulsmodding/csplugin/EventListener.java
@@ -0,0 +1,7 @@
+package com.stpaulsmodding.csplugin;
+
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener {
+	
+}
