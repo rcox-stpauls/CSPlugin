f5f3073d34a085eae69cc06333dff4c6509fa611
stpauls-modding <23203@stpauls.qld.edu.au>
2015-10-14 18:21:42 +1000

    Added functionality for saving blocks to a file.

--- src/com/stpaulsmodding/csplugin/CSPlugin.java
+++ src/com/stpaulsmodding/csplugin/CSPlugin.java
@@ -1,15 +1,37 @@
 package com.stpaulsmodding.csplugin;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CSPlugin extends JavaPlugin {
+	private PrintWriter printwriter;
+	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new EventListener(), this);
+		getServer().getPluginManager().registerEvents(new EventListener(this), this);
+		getCommand("").setExecutor(new PluginCommands(this));
+		try {
+			File file = new File(getDataFolder(), "cached_blocks.log");
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			printwriter = new PrintWriter(new FileWriter(file, true));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		printwriter.close();
+	}
+	
+	public void saveBlocks(String data) {
+		printwriter.print(data);
+		printwriter.flush();
 	}
 }
