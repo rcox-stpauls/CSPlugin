2ba7fc5db20b3d30fbe9655c77efaa3f25b63256
stpauls-modding <23203@stpauls.qld.edu.au>
2015-10-14 18:23:00 +1000

    Blanked.

--- src/com/stpaulsmodding/csplugin/EventListener.java
+++ src/com/stpaulsmodding/csplugin/EventListener.java
@@ -1,38 +1,12 @@
 package com.stpaulsmodding.csplugin;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
+	@SuppressWarnings("unused")
+	private final CSPlugin plugin;
 	
-	@EventHandler
-	public void onDamage(EntityDamageEvent event) {
-		if (event.getEntity() instanceof Player) {
-			Player player = (Player) event.getEntity();
-			if (event.getCause().equals(DamageCause.FIRE)) {
-				for (ItemStack set : player.getInventory().getArmorContents()) {
-					switch(set.getType()) {
-						case LEATHER_BOOTS:
-							set.setDurability((short) (set.getDurability() - 5));
-							break;
-						case LEATHER_LEGGINGS:
-							set.setDurability((short) (set.getDurability() - 6));
-							break;
-						case LEATHER_CHESTPLATE:
-							set.setDurability((short) (set.getDurability() - 6));
-							break;
-						case LEATHER_HELMET:
-							set.setDurability((short) (set.getDurability() - 4));
-							break;
-						default:
-							break;
-					}	
-				}
-			}
-		}
+	public EventListener(CSPlugin plugin) {
+		this.plugin = plugin;
 	}
 }
