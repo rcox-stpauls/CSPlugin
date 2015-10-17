12329cdcf0cdca02863acd097e091e50519fb85d
stpauls-modding <23203@stpauls.qld.edu.au>
2015-10-17 10:03:29 +1000

    Added Saving/Loading of Ships

--- src/com/stpaulsmodding/csplugin/CSPlugin.java
+++ src/com/stpaulsmodding/csplugin/CSPlugin.java
@@ -1,37 +1,95 @@
 package com.stpaulsmodding.csplugin;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CSPlugin extends JavaPlugin {
-	private PrintWriter printwriter;
+	private ObjectOutputStream writer;
+	private ObjectInputStream reader;
+	private File file;
 	
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new EventListener(this), this);
-		getCommand("").setExecutor(new PluginCommands(this));
+		getCommand("airships").setExecutor(new PluginCommands(this));
 		try {
-			File file = new File(getDataFolder(), "cached_blocks.log");
+			file = new File(getDataFolder(), "cached_blocks.log");
 			if (!file.exists()) {
 				file.createNewFile();
 			}
-			printwriter = new PrintWriter(new FileWriter(file, true));
-		} catch (IOException e) {
+			writer = new ObjectOutputStream(new FileOutputStream(file));
+			reader = new ObjectInputStream(new FileInputStream(file));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	@Override
 	public void onDisable() {
-		printwriter.close();
+		try {
+			writer.close();
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void saveBlocks(String data) {
-		printwriter.print(data);
-		printwriter.flush();
+	public void saveTo(Map<Location, Block> data) {
+		try {
+			writer.writeObject(data);
+			writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void wipeFile() {
+		file.delete();
+		try {
+			file.createNewFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Map<Location, Block> loadFile() {
+		try {
+			@SuppressWarnings("unchecked")
+			Map<Location, Block> obj = (Map<Location, Block>) reader.readObject();
+			return obj;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Map<Location, Block> getBlocksFromSelection(World world, Location loc1, Location loc2) {
+		Map<Location, Block> items = new HashMap<Location, Block>();
+		for (int x = loc1.getBlockX(); x < loc2.getBlockX(); x++) {
+			for (int y = loc1.getBlockY(); y < loc2.getBlockY(); y++) {
+				for (int z = loc1.getBlockZ(); z < loc2.getBlockZ(); z++) {
+					items.put(new Location(world, x, y, z), world.getBlockAt(x, y, z));
+				}
+			}
+		}
+		return items;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void loadBlocksFromHashMap(Map<Location, Block> map, Location loc) {
+		for (Location blockloc : map.keySet()) {
+			Block toChange = loc.getWorld().getBlockAt(loc.getBlockX()+blockloc.getBlockX(), loc.getBlockY()+blockloc.getBlockY(), loc.getBlockZ()+blockloc.getBlockZ());
+			toChange.setData(map.get(blockloc).getData());
+		}
 	}
 }
--- src/com/stpaulsmodding/csplugin/PluginCommands.java
+++ src/com/stpaulsmodding/csplugin/PluginCommands.java
@@ -1,12 +1,15 @@
 package com.stpaulsmodding.csplugin;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PluginCommands implements CommandExecutor {
-	@SuppressWarnings("unused")
 	private final CSPlugin plugin;
+	private Location firstSelection;
+	private Location secondSelection;
 	
 	public PluginCommands(CSPlugin plugin) {
 		this.plugin = plugin;
@@ -15,7 +18,26 @@ public class PluginCommands implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("airships")) {
+			if (!(sender instanceof Player)) {
+				return false;
+			}
+			if (args.length < 1) {
+				return false;
+			}
+			Player player = (Player) sender;
 			
+			if (args[0].equalsIgnoreCase("setfirst")) {
+				firstSelection = player.getLocation();
+				player.sendMessage("First location set.");
+			} else if (args[0].equalsIgnoreCase("setsecond")) {
+				secondSelection = player.getLocation();
+				player.sendMessage("Second location set.");
+			} else if (args[0].equalsIgnoreCase("saveship")) {
+				plugin.wipeFile();
+				plugin.saveTo(plugin.getBlocksFromSelection(player.getWorld(), firstSelection, secondSelection));
+			} else if (args[0].equalsIgnoreCase("loadship")) {
+				plugin.loadBlocksFromHashMap(plugin.loadFile(), player.getLocation());
+			}
 		}
 		
 		return false;
